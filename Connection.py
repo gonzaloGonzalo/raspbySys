75f0a7f1eb0cb51a63f799a61e43241c27fd5a36
GJC <gonza_garay@hotmail.com>
2017-04-14 01:20:09 -0300

    Add more DAO classes.

--- Connection.py
+++ Connection.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData, Column, Table, ForeignKey
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, VARCHAR
 from model import Users
 
 engine = create_engine('sqlite:///raspBy.db',
@@ -10,20 +10,22 @@ metadata = MetaData(bind=engine)
 
 
 
-# addresses_table = Table('addresses', metadata,
-#                        Column('id', Integer, primary_key=True),
-#                        Column('user_id', None, ForeignKey('users.id')),
-#                        Column('email_address', String, nullable=False)
-#                        )
+addresses_table = Table('position', metadata,
+                        Column('id', Integer, primary_key=True),
+                        Column('name', VARCHAR, nullable=False),
+                        Column('created_on',  VARCHAR, nullable=False),
+                        Column('latitude',  VARCHAR, nullable=False),
+                        Column('longitude',  VARCHAR, nullable=False)
+                        )
 
 # create an Insert object
-ins = users_table.insert()
+#ins = users_table.insert()
 # add values to the Insert object
-new_user = ins.values(name="Joe", age=20, password="pass")
+#new_user = ins.values(name="Joe", age=20, password="pass")
 
 # create a database connection
-conn = engine.connect()
+#conn = engine.connect()
 # add user to database by executing SQL
-conn.execute(new_user)
+#conn.execute(new_user)
 
-# metadata.create_all()
+metadata.create_all()
